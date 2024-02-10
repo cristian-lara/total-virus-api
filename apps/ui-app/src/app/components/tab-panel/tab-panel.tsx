46ba27d180c5d0ca17b4a72c6871ad0d6f24ce57
Cristian Lara <cristian89lara@gmail.com>
2024-02-10 12:42:09 -0800

    added tab panel

---
+++ apps/ui-app/src/app/components/tab-panel/tab-panel.tsx
@@ -0,0 +1,29 @@
+import { Box, Typography } from '@mui/material';
+import styles from './tab-panel.module.scss';
+
+/* eslint-disable-next-line */
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const TabPanel = ({ children, value, index, ...other }: TabPanelProps) => {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+export default TabPanel;
--- apps/ui-app/src/app/perform-report/page.tsx
+++ apps/ui-app/src/app/perform-report/page.tsx
@@ -1,10 +1,82 @@
-import { Grid } from '@mui/material';
-import styles from './page.module.scss';
+import { Box, Container, Grid, Paper, Tab, Tabs, Typography, useMediaQuery, useTheme } from '@mui/material';
+import TabPanel from '../components/tab-panel/tab-panel';
+import React, { useState } from 'react';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+
 
 /* eslint-disable-next-line */
 export interface PerformReportProps {}
 
 export default function PerformReport(props: PerformReportProps) {
+  const [value, setValue] = useState(0);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const tabStyle = {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    marginBottom: '24px',
+  };
+
+  const contentStyle = {
+    border: `1px solid ${theme.palette.divider}`,
+    minHeight: '200px',
+    padding: isMobile ? '16px' : '24px',
+  };
+
   return (
-<h1>holaa</h1>  );
+    <Container maxWidth="lg">
+      <Paper elevation={3} sx={{ maxWidth: '100%', margin: 'auto', mt: 4, p: isMobile ? 1 : 2, borderLeft: '6px solid #0073ff',
+        borderBottom: '6px solid #0073ff', }}>
+        <Box sx={{
+          position: 'relative',
+          paddingRight: '40px',
+          marginBottom: '20px'
+        }}>
+          <Grid container spacing={2} sx={{ alignItems: 'center', marginLeft: isMobile ? 1 : '20px' }}>
+            <Grid item xs={12} sm={9}>
+              <Typography variant="h5" component="h2" gutterBottom>
+                Perform Report
+              </Typography>
+              <Typography variant="subtitle1">
+                This section allows you to perform detailed security reports.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={3} sx={{ textAlign: 'center' }}>
+              <AssessmentIcon sx={{
+                fontSize: '4.5rem',
+                color: 'action.active',
+                verticalAlign: 'bottom', // Alinea el ícono con el texto
+              }} />
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+      <Box sx={{ width: '100%', padding: isMobile ? 1 : '24px', mt: 5, bgcolor: 'background.paper' }}>
+        {/* Tabs aquí */}
+        <Box sx={tabStyle}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" variant="fullWidth">
+            <Tab label="URL" />
+            <Tab label="Search" />
+            <Tab label="File" />
+          </Tabs>
+        </Box>
+        {/* Contenido de las tabs aquí */}
+        <Box sx={contentStyle}>
+          <TabPanel value={value} index={0}>
+            Content for URL tab
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Content for Search tab
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            Content for File tab
+          </TabPanel>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
